11a469f1ab197ab3f5a9dd6ffa622948c901b24f
Chris Karvouniaris <christos.karvouniaris247@gmail.com>
2024-03-20 19:03:38 +0200

    linting

--- retry/_exceptions.py
+++ retry/_exceptions.py
@@ -1,6 +1,12 @@
-MAX_RETRIES_MESSAGE_TEMPLATE = "Have reached max number of retries ({max_retries}) for function {fname}, aborting."
-TIMEOUT_MESSAGE_TEMPLATE = "Have been retrying function {fname} for {elapsed_time} secs. Exceeded timeout of {timeout} secs, aborting."
-DEADLINE_MESSAGE_TEMPLATE = "Have been retrying function {fname} for {elapsed_time} secs. Exceeded deadline of {deadline} secs, aborting."
+MAX_RETRIES_MESSAGE_TEMPLATE = (
+    "Have reached max number of retries ({max_retries}) for function {fname}, aborting."
+)
+TIMEOUT_MESSAGE_TEMPLATE = (
+    "Have been retrying function {fname} for {elapsed_time} secs. Exceeded timeout of {timeout} secs, aborting."
+)
+DEADLINE_MESSAGE_TEMPLATE = (
+    "Have been retrying function {fname} for {elapsed_time} secs. Exceeded deadline of {deadline} secs, aborting."
+)
 
 
 class BaseRetryException(Exception):
@@ -13,10 +19,9 @@ class BaseRetryException(Exception):
 
 
 class MaxRetriesException(BaseRetryException):
-    def __init__(self, logger, fname, failure_callback,  max_retries):
+    def __init__(self, logger, fname, failure_callback, max_retries):
         message = MAX_RETRIES_MESSAGE_TEMPLATE.format(
-            fname=fname,
-            max_retries=max_retries
+            fname=fname, max_retries=max_retries
         )
         super().__init__(logger, message, failure_callback)
 
@@ -24,18 +29,14 @@ class MaxRetriesException(BaseRetryException):
 class RetriesTimeoutException(BaseRetryException):
     def __init__(self, logger, fname, failure_callback, elapsed_time: float, timeout: float):
         message = TIMEOUT_MESSAGE_TEMPLATE.format(
-            fname=fname,
-            elapsed_time=elapsed_time,
-            timeout=timeout
+            fname=fname, elapsed_time=elapsed_time, timeout=timeout
         )
         super().__init__(logger, message, failure_callback)
 
 
 class RetriesDeadlineException(BaseRetryException):
-    def __init__(self, logger, fname, failure_callback,  elapsed_time: float, deadline: float):
+    def __init__(self, logger, fname, failure_callback, elapsed_time: float, deadline: float):
         message = DEADLINE_MESSAGE_TEMPLATE.format(
-            fname=fname,
-            elapsed_time=elapsed_time,
-            deadline=deadline
+            fname=fname, elapsed_time=elapsed_time, deadline=deadline
         )
         super().__init__(logger, message, failure_callback)
