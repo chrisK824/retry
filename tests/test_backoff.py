f070ad2fad0c6d488da17aff4cd75b2206d891fd
Chris Karvouniaris <christos.karvouniaris247@gmail.com>
2024-05-21 18:41:52 +0300

    added some test parameters for linear and exponential backoff on max argument for capping max delay

--- tests/test_backoff.py
+++ tests/test_backoff.py
@@ -32,28 +32,38 @@ def test_fixed_backoff(base_delay, jitter):
 
 
 @pytest.mark.parametrize(
-    "base_delay, step, jitter",
+    "base_delay, step, jitter, max_delay",
     [
-        (1.0, 0.5, None),
-        (1.0, 0.5, (0.1, 0.5)),
-        (0.0, 0.0, None),
-        (0.0, 0.0, (0.0, 0.0)),
+        (1.0, 0.5, None, None),
+        (1.0, 0.5, (0.1, 0.5), None),
+        (0.0, 0.0, None, None),
+        (0.0, 0.0, (0.0, 0.0), None),
+        (1.0, 0.5, None, 2.0),
+        (1.0, 0.5, (0.1, 0.5), 2.0),
     ],
 )
-def test_linear_backoff(base_delay, step, jitter):
-    backoff = LinearBackOff(base_delay, step, jitter)
+def test_linear_backoff(base_delay, step, jitter, max_delay):
+    backoff = LinearBackOff(base_delay, step, jitter, max_delay)
 
     for _round in range(5):
         delay = backoff.delay
         assert isinstance(delay, float)
-        expected_delay = base_delay + (step * _round)
+
+        if max_delay is not None:
+            expected_delay = min(base_delay + (step * _round), max_delay)
+        else:
+            expected_delay = base_delay + (step * _round)
+
         if jitter is None:
             assert delay == expected_delay
         else:
             low_jitter = jitter[0] if _round else 0
             high_jitter = jitter[1] if _round else 0
-            assert expected_delay + low_jitter <= delay
-            assert delay <= expected_delay + high_jitter
+            if max_delay is None:
+                assert expected_delay + low_jitter <= delay
+                assert delay <= expected_delay + high_jitter
+            else:
+                assert delay <= max_delay
 
 
 @pytest.mark.parametrize(
@@ -77,28 +87,39 @@ def test_random_uniform_backoff(base_delay, min_delay, max_delay):
 
 
 @pytest.mark.parametrize(
-    "base_delay, base, jitter",
+    "base_delay, base, jitter, max_delay",
     [
-        (1.0, None, None),
-        (1.0, None, (0.1, 0.5)),
-        (0.0, None, None),
-        (0.0, None, (0.0, 0.0)),
+        (1.0, None, None, None),
+        (1.0, None, (0.1, 0.5), None),
+        (0.0, None, None, None),
+        (0.0, None, (0.0, 0.0), None),
+        (1.0, None, None, 10.0),
+        (1.0, None, (0.1, 0.5), 10.0),
     ],
 )
-def test_exponential_backoff(base_delay, base, jitter):
-    backoff = ExponentialBackOff(base_delay, base, jitter)
+def test_exponential_backoff(base_delay, base, jitter, max_delay):
+    backoff = ExponentialBackOff(base_delay, base, jitter, max_delay)
 
     for _round in range(5):
         delay = backoff.delay
         assert isinstance(delay, float)
-        expected_delay = base_delay * (2**_round)
+        base_value = base if base is not None else 2
+
+        if max_delay is not None:
+            expected_delay = min(base_delay * (base_value**_round), max_delay)
+        else:
+            expected_delay = base_delay * (base_value**_round)
+
         if jitter is None:
             assert delay == expected_delay
         else:
             low_jitter = jitter[0] if _round else 0
             high_jitter = jitter[1] if _round else 0
-            assert expected_delay + low_jitter <= delay
-            assert delay <= expected_delay + high_jitter
+            if max_delay is None:
+                assert expected_delay + low_jitter <= delay
+                assert delay <= expected_delay + high_jitter
+            else:
+                assert delay <= max_delay
 
 
 def test_fixed_backoff_invalid_jitter():
